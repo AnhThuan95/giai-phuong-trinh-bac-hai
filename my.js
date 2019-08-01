50aa0d4e74d059242b25736bf94a41570cc3931f
AnhThuan95 <thuan.hust.95@gmail.com>
2019-08-01 10:24:59 +0700

    message

---
+++ my.js
@@ -0,0 +1,23 @@
+function giaiPhuongTrinh() {
+    let a = parseInt(document.getElementById("a").value);
+    let b = parseInt(document.getElementById("b").value);
+    let c = parseInt(document.getElementById("c").value);
+
+    if (a == 0){
+        document.getElementById("result").innerHTML = "Day khong phai phuongtrinh bac 2!";
+    } else {
+        delta = b * b - 4 * a * c;
+        if (delta < 0){
+            document.getElementById("result").innerHTML = "Phuong trinh vo nghiem."
+        } else {
+            if (delta == 0){
+                x = -b / (2 * a);
+                document.getElementById("result").innerHTML = "Phuong trinh co nghiem kep: " + x;
+            } else {
+                x1 = (-b + Math.sqrt(delta)) / (2 * a);
+                x2 = (-b - Math.sqrt(delta)) / (2 * a);
+                document.getElementById("result").innerHTML = "Phuong trinh co 2 nghiem:</br> x1 = " + x1 + "</br> x2 = " +x2;
+            }
+        }
+    }
+}
